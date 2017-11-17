4966c8ce21489b2d6437c97b4d0556003806eef6
Nicholas Frederick <nicholasfrederick@SupportITs-MacBook-Pro.local>
2017-11-17 18:15:17 +0700

    Change code to rely on user input

--- partnerapi/exchange-token.sh
+++ partnerapi/exchange-token.sh
@@ -2,27 +2,27 @@
 
 # Partner API
 # Exchange token from short to long live token
-# ./excchange-token.sh <client-id> <client-secreet> <access-token>
+# ./exchange-token.sh <client-id> <client-secreet> <access-token>
 # Access token could be acquired from running pm2 log
-ACCESS_TOKEN="eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIiwia2lkIjoiYmJtdHNfZW5jX2tleV9iZXRhIiwiY3R5IjoiSldUIn0..GzfsVHagPXqk52Ta.4ps5gZvJjhEurCLPWLFkgpg-7HVABTYiOIPLkl0J8mehhB171rRKBbsGBXr3EWfKjkh6b0c50F9oaJZ1P06sblpipIN3fhlUpMS96r92Vo6bfOV1Cdc52yaNcE7YOHKhnCygSoEisTaTZBGkrAG2acNjtkw9ppZ9uKMc9TvEPWIh9dJ9C8z4I9MHRkbaKTEJkyGo3oRAyo-bdiVbtHShgSNCQ8QNKSG9kL4pczO4leZuf8ZlIJFpHgdv80M6CCYql6gJxS4YL3ro7sG0iN3X95vNuYFMg1N6G5wB9qCPg4rpR9lqUNn1iSqVECwIAPfocc307LKM1cHJH-6ipff4ejBzMfQQVmO97VSpu4oRoSDJ8aEfImBYcsI8q4rf6McXegWI_i13E1Pv38_WfuPZXdPZ5MCQ7QyAwkGNT8mZjriZMD6LwS3YFfTvaQ.0q9vLWir02p1Np1IxXJaiA"
-
 # BASE64 is encoded code from "oauthUsername:oauthPassword" that could be acquired from .env file
-BASE64="WDZEMjRUaEtZYzBwRHFlNGtFOjFqVnc1Y0wwc1NOenFxeG9OZkxHV3E0UUVnNmxoYWhQZ0IzU3hWMG9WQnBX"
-
-#
 
 echo This will exhchange short-lived token to long-lived token.
-
-
-echo Please enter Client ID
+echo 'Please enter Client ID'
 read clientID
-echo Please enter Client Secreet
-read clientSecret
-echo Please enter Short-Leved token
+echo 'Please enter Client Secreet'
+read clientSecreet
+combined=$clientID:$clientSecreet
+baseEncoded64=`printf $combined | base64 | tr -d '[:space:]'`
+echo $combined
+echo 'Please enter Short-l token'
 read slToken
-echo Please enter path to your Private Key file (.key)
+echo 'below is your base64'
+echo $baseEncoded64
+echo 'Please enter path to your Private Key file (.key)'
+echo 'Example /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.key'
 read key
-echo Please enter path to your Cert file (.crt)
+echo 'Please enter path to your Cert file (.crt)'
+echo 'Example /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.crt'
 read crt
 
-curl -v --cert /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.crt --key /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.key -H "Content-Type:application/x-www-form-urlencoded" -H "Accept: application/json" -H "Authorization: Basic $BASE64" -d "grant_type=exchange_token&access_token=$ACCESS_TOKEN" "https://auth-beta.bbm.blackberry.com:8443/oauth/token"
+curl -v --cert $crt --key $key -H "Content-Type:application/x-www-form-urlencoded" -H "Accept: application/json" -H "Authorization: Basic $baseEncoded64" -d "grant_type=exchange_token&access_token=$slToken" "https://auth-beta.bbm.blackberry.com:8443/oauth/token"
