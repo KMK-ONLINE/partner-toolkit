538677eb4b2a45ec591c6b0e9a6866f8d939bf2a
Nicholas Frederick <nicholasfrederick@SupportITs-MacBook-Pro.local>
2017-11-21 09:29:25 +0700

    rename .sh files and refactoring

--- partnerapi/get_current_user.sh
+++ partnerapi/get_current_user.sh
@@ -6,6 +6,6 @@
 # Access token could be acquired from running pm2 log
 echo 'This will return current user profile from exchange token.'
 echo 'Please enter your Access Token (bearer abc)'
-read access_token
+read accessToken
 
-curl -v -H "Authorization:$access_token" 'https://api-beta.bbm.blackberry.com:8443/v2/user/profile'
+curl -v -H "Authorization:$accessToken" 'https://api-beta.bbm.blackberry.com:8443/v2/user/profile'
--- partnerapi/get_current_user_contact.sh
+++ partnerapi/get_current_user_contact.sh
@@ -6,6 +6,6 @@
 # Access token could be acquired from running pm2 log
 echo 'This will return bbid of current user contact.'
 echo 'Please enter your Access Token (bearer abc)'
-read access_token
+read accessToken
 
-curl -v -H "Authorization:$access_token" 'https://api-beta.bbm.blackberry.com:8443/v2/user/contacts'
+curl -v -H "Authorization:$accessToken" 'https://api-beta.bbm.blackberry.com:8443/v2/user/contacts'
