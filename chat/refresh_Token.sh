64f95ce2f67a81ee143146a27a9313df67b44d4e
AshishGloballogic <33252718+AshishGloballogic@users.noreply.github.com>
2017-11-22 14:47:11 +0530

    Update refresh_Token.sh

--- chat/refresh_Token.sh
+++ chat/refresh_Token.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-# Partner API
-# Refresh Token
+printf 'Partner API\n'
+printf 'Refresh token\n'
 # ./exchange-token.sh <client-id> <client-secreet> <access-token>
-# Refresh Token could be acquired from running pm2 log
-# BASE64 is encoded code from "oauthUsername:oauthPassword" that could be acquired from .env file
+printf 'Access token could be acquired from running pm2 log\n'
+printf 'BASE64 is encoded code from "oauthUsername:oauthPassword" that could be acquired from .env file in the server\n'
 
-echo This will exhchange short-lived token to long-lived token.
-echo 'Please enter Client ID'
+printf 'This will exhchange short-lived token to long-lived token.\n'
+printf 'Please enter clientId/oauthUsername\n'
 read clientID
-echo 'Please enter Client Secreet'
+printf 'Please enter clientSecreet/oauthPassword\n'
 read clientSecreet
+# translate to base64
 combined=$clientID:$clientSecreet
 baseEncoded64=`printf $combined | base64 | tr -d '[:space:]'`
-echo $combined
-echo 'Please enter refresh_token acquired in previous token request'
+printf 'below is your base64\n'
+printf $baseEncoded64
+
+printf '\nPlease enter refresh_token that\n'
+printf 'Acquired from exchange_token.sh\n'
 read refreshToken
-echo 'below is your base64'
-echo $baseEncoded64
-echo 'Please enter path to your Private Key file (.key)'
-echo 'Example /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.key'
+printf '\nPlease enter path to your Private Key file (.key)\n'
+printf 'Example /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.key\n'
 read key
-echo 'Please enter path to your Cert file (.crt)'
-echo 'Example /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.crt'
+printf '\nPlease enter path to your Cert file (.crt)\n'
+printf 'Example /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.crt\n'
 read crt
 
-curl -v --cert $crt --key $key -H "Content-Type:application/x-www-form-urlencoded" -H "Accept: application/json" -H "Authorization: Basic $baseEncoded64" -d "grant_type=refresh_token&refreshToken=$refreshToken&scope=v1" "https://auth-beta.bbm.blackberry.com:8443/oauth/token"
+curl -v --cert $crt --key $key -H "Content-Type:application/x-www-form-urlencoded" -H "Accept: application/json" -H "Authorization: Basic $baseEncoded64" -d "grant_type=refresh_token&refresh_token=$refreshToken" "https://auth-beta.bbm.blackberry.com:8443/oauth/token"
