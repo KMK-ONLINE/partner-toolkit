617d89caddd619e308a26df43ee23028844152d9
Ashish Sharma <ashishSharma@github.com>
2017-11-23 16:10:10 +0530

    Some Changes

--- chat/access_token.sh
+++ chat/access_token.sh
@@ -1,27 +1,23 @@
 #/bin/bash
-
 # Chat API
 # Getting Client Credentials from Access Token
 # ./exchange-token.sh <client-id> <client-secreet> <access-token>
 # Access token can be acquired from running "pm2 log"
 # BASE64 is encoded code from "oauthUsername:oauthPassword" that could be acquired from .env file
-
-echo This will get the Clint Credentials from the Access Token
-
-echo 'Please enter Client ID'
+echo 'This will get the Clint Credentials from the Access Token\n'
+echo 'Please enter Client ID\n'
 read clientID
-echo 'Please enter Client Secreet'
+echo 'Please enter Client Secret\n'
 read clientSecreet
 combined=$clientID:$clientSecreet
 baseEncoded64=`printf $combined | base64 | tr -d '[:space:]'`
 echo $combined
-echo 'below is your base64'
+echo 'below is your base64\n'
 echo $baseEncoded64
-echo 'Please enter path to your Private Key file (.key)'
-echo 'Example /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.key'
+echo 'Please enter path to your Private Key file (.key)\n'
+echo 'Example /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.key\n'
 read key
-echo 'Please enter path to your Cert file (.crt)'
-echo 'Example /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.crt'
+echo 'Please enter path to your Cert file (.crt)\n'
+echo 'Example /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.crt\n'
 read crt
-
 curl -v --cert $crt --key $key -H "Content-Type:application/x-www-form-urlencoded" -H "Accept: application/json" -H "Authorization: Basic $baseEncoded64" -d "grant_type=client_credentials" "https://auth-beta.bbm.blackberry.com:8443/oauth/token"
--- chat/refresh_token.sh
+++ chat/refresh_token.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-
 printf 'Partner API\n'
 printf 'Refresh token\n'
 # ./exchange-token.sh <client-id> <client-secreet> <access-token>
 printf 'Access token could be acquired from running pm2 log\n'
 printf 'BASE64 is encoded code from "oauthUsername:oauthPassword" that could be acquired from .env file in the server\n'
-
 printf 'This will exhchange short-lived token to long-lived token.\n'
 printf 'Please enter clientId/oauthUsername\n'
 read clientID
@@ -16,7 +13,6 @@ combined=$clientID:$clientSecreet
 baseEncoded64=`printf $combined | base64 | tr -d '[:space:]'`
 printf 'below is your base64\n'
 printf $baseEncoded64
-
 printf '\nPlease enter refresh_token that\n'
 printf 'Acquired from exchange_token.sh\n'
 read refreshToken
@@ -26,5 +22,4 @@ read key
 printf '\nPlease enter path to your Cert file (.crt)\n'
 printf 'Example /app/demo-server/nodejs/ssl/bbmmobilenews.com_thawte.crt\n'
 read crt
-
 curl -v --cert $crt --key $key -H "Content-Type:application/x-www-form-urlencoded" -H "Accept: application/json" -H "Authorization: Basic $baseEncoded64" -d "grant_type=refresh_token&refresh_token=$refreshToken" "https://auth-beta.bbm.blackberry.com:8443/oauth/token"
